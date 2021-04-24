dc372d63418c384727adbdba826ea995d3814249
RedVeil <leon.niesler@gmail.com>
2021-04-24 14:54:05 +0200

    init react app

---
+++ app/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function App():JSX.Element {
+  return (
+    <div className="bg-gray-50">
+    </div>
+  );
+}
---
+++ app/src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ChainId, DAppProvider } from '@usedapp/core'
+import App from './App'
+
+const config = {
+  readOnlyChainId: ChainId.Mainnet,
+  readOnlyUrls: {
+    [ChainId.Mainnet]: 'https://mainnet.infura.io/v3/62687d1a985d4508b2b7a24827551934',
+  },
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
