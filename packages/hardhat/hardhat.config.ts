30dfae4dbbf40be934d1b07c9aba01aa89a3d3fb
Anthony Martin <anthony@replaycreative.com>
2021-05-09 14:53:14 +0200

    adds hardhat tasks for convenience

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,4 +1,5 @@
 // Plugins
+require("dotenv").config({ path: "../../.env" });
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import "hardhat-typechain";
@@ -14,8 +15,123 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
   }
 });
 
+
+
+  task("l2:oDAI:balance", "checks balance of account")
+  .addParam('address', 'address to check balance of')
+  .setAction(async (args, hre) => {
+    const accounts = await hre.ethers.getSigners();
+
+    const oDAI = new hre.ethers.Contract(
+      process.env.REACT_APP_L2_DAI_ADDRESS,
+      require("./artifacts-ovm/contracts/L2DepositedERC20.sol/L2DepositedERC20.json").abi,
+      accounts[0]
+    );
+    console.log("balance", (await oDAI.balanceOf(args.address)).toString());
+  });
+
+  task("l1:pool:properties")
+  .setAction(async(args, hre) => {
+    const accounts = await hre.ethers.getSigners();
+    const pool = new hre.ethers.Contract(
+      process.env.REACT_APP_L1_POOL_ADDRESS,
+      require("./artifacts/contracts/L1_Pool.sol/L1_Pool.json").abi,
+      accounts[0]
+    );
+
+    console.log({
+      DAI: await pool.dai(),
+      crvLPToken: await pool.crvLPToken(),
+      YearnVault: await pool.yearnVault(),
+      CurveDepositZap: await pool.curveDepositZap(),
+      L1_ERC20Gateway: await pool.L1_ERC20Gateway(),
+      PoolTokenEscrow: await pool.poolTokenEscrow(),
+      RewardsManager: await pool.rewardsManager(),
+      L2Pool: await pool.L2_Pool()
+    })
+
+  });
+
+
+  task("l1:pool:deposit", "withdraws from L1 pool")
+  .setAction( async (args, hre) => {
+    const accounts = await hre.ethers.getSigners();
+    const pool = new hre.ethers.Contract(
+      process.env.REACT_APP_L1_POOL_ADDRESS,
+      require("./artifacts/contracts/L1_Pool.sol/L1_Pool.json").abi,
+      accounts[0]
+    );
+
+    const response = await pool.deposit();
+    console.log(response);
+  });
+
+  task("l1:pool:withdraw", "withdraws from L1 pool")
+  .addParam('amount', 'amount to withdraw')
+  .addParam('withdrawto', 'address to withdraw to')
+  .addParam('batchid', 'batch id')
+  .setAction( async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  const pool = new hre.ethers.Contract(
+    process.env.REACT_APP_L1_POOL_ADDRESS,
+    require("./artifacts/contracts/L1_Pool.sol/L1_Pool.json").abi,
+    accounts[0]
+  );
+
+  const response = await pool.withdraw(
+    args.amount, 
+    args.withdrawto, 
+    args.batchid
+  );
+  console.log(response);
+});
+
+task("l1:pool:balance", "checks balance of account")
+  .addParam('address')
+  .setAction(async (args, hre) => {
+    const accounts = await hre.ethers.getSigners();
+
+    const pool = new hre.ethers.Contract(
+      process.env.REACT_APP_L1_POOL_ADDRESS,
+      require("./artifacts/contracts/L1_Pool.sol/L1_Pool.json").abi,
+      accounts[0]
+    );
+
+    console.log("balance", (await pool.balanceOf(args.address)).toString());
+  });
+
+
+  task("l2:pool:properties")
+  .setAction(async(args, hre) => {
+    const accounts = await hre.ethers.getSigners();
+    const pool = new hre.ethers.Contract(
+      process.env.REACT_APP_L2_POOL_ADDRESS,
+      require("./artifacts-ovm/contracts/L2_Pool.sol/L2_Pool.json").abi,
+      accounts[0]
+    );
+
+    console.log({
+      oDAI: await pool.oDAI(),
+      L1_Pool: await pool.L1_Pool(),
+      toDeposit: (await pool.toDeposit()).toString(),
+      batchTransferPeriod: (await pool.batchTransferPeriod()).toString(),
+      lastDepositMadeAt: (await pool.lastDepositMadeAt()).toString(),
+      L1_Target: await pool.L1_Target(),
+      currentWithdrawalBatchId: await pool.currentWithdrawalBatchId(),
+      withdrawalPeriod: (await pool.withdrawalPeriod()).toString(),
+      lastWithdrawalMadeAt: (await pool.lastWithdrawalMadeAt()).toString()
+    })
+
+  });
+
 module.exports = {
   networks: {
+    localhost: {
+      url: "http://localhost:9545",
+      accounts: {
+        mnemonic: 'test test test test test test test test test test test junk' 
+      }
+    },
     hardhat: {
       accounts: {
         mnemonic: 'test test test test test test test test test test test junk'
